c75ea2d66315292055c7883d56ec6ef639540b09
SudoMock <hello@sudomock.com>
2026-03-15 02:27:10 +0300

    fix: sidebar icon path was pointing to deleted .svg, now .png

--- includes/class-sudomock-admin.php
+++ includes/class-sudomock-admin.php
@@ -58,7 +58,7 @@ final class SudoMock_Admin {
             'manage_woocommerce',
             'sudomock-settings',
             array( $this, 'render_page' ),
-            SUDOMOCK_PLUGIN_URL . 'assets/images/icon-20.svg',
+            SUDOMOCK_PLUGIN_URL . 'assets/images/icon-20.png',
             58
         );
     }
